aa31f2ba5819accb15f7ed64b5d552d3921ea00c
Deepak Sharma <deepaksharma89738@gmail.com>
2026-03-02 10:57:44 +0530

    Add files via upload

---
+++ Program to distribute the grade based on marks A,B,C,D,E and F.C
@@ -0,0 +1,33 @@
+/*Program to Distribute the grade based on marks A,B,C,D,E,F*/
+#include<stdio.h>
+int main()
+{
+    int marks;
+    printf("Enter marks\n");
+    scanf("%d", &marks);
+    if (marks >= 90) 
+    {
+        printf("Grade A\n");
+    } 
+    else if (marks >= 80) 
+    {
+        printf("Grade B\n");
+    } 
+    else if (marks >= 70) 
+    {
+        printf("Grade C\n");
+    } 
+    else if (marks >= 60) 
+    {
+        printf("Grade D\n");
+    } 
+    else if (marks >= 50) 
+    {
+        printf("Grade E\n");
+    }
+    else 
+    {
+        printf("Grade F\n");
+    }
+    return 0;
+}
